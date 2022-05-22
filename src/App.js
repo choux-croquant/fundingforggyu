7e503d026adfb4ca6857f0299fb680320149bae6
Choux <choifrance@gmail.com>
2022-05-22 20:47:53 +0900

    update : banner img style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className={styles.App}>
-      <header>
+      <header className={styles.headerStyle}>
         <Header></Header>
       </header>
       <section className={styles.infoContainer}>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,11 @@
 import { memo } from "react"
 import styles from "./Header.module.css"
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel"
 
 const Header = () => {
 
   return (
     <div className={styles.container}>
-      <Carousel className={styles.carousel}
-        autoPlay={true}
-        infiniteLoop={true}
-        showArrows={false}
-        showStatus={false}
-        showIndicators={false}
-        showThumbs={false}>
-        <div className={styles.slide}>
-          <img src="/fundingBanner.png" alt="생일 축하 펀딩을 알리는 광고" />
-        </div>
-      </Carousel>
+        <img className={styles.bannerImg} src="/fundingBanner.png" alt="생일 축하 펀딩을 알리는 광고" />
     </div>
   )
 }
